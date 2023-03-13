bc77e93a1f6c814e6c183d4033787cc3729e99b7
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-03-13 11:10:12 +0100

    Finish all definitions

--- src/fasterqc/_qcmodule.c
+++ src/fasterqc/_qcmodule.c
@@ -21,6 +21,53 @@ along with fasterqc.  If not, see <https://www.gnu.org/licenses/
 
 PyTypeObject *SequenceRecord;
 
+/* Nice trick from fastp: A,C, G, T, N all have different last three
+   bits. So this requires only 8 entries per count array. Fastp performs
+   a bitwise and of 0b111 on every character.
+   This can be taken further by using  a lookup table. A=1, C=2, G=3, T=4.
+   Lowercase a,c,g and t are supported. All other characters are index 0 and
+   are considered N. This way we can make a very dense count table, and don't
+   have to check every nucleotide if it is within bounds. Furthermore, odd
+   characters such as IUPAC K will map to N, unlike the fastp method where K
+   will map to C. */
+
+const uint8_t NUCLEOTIDE_TO_INDEX[128] = {
+// Control characters
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+// Interpunction numbers etc
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//  A, B, C, D, E, F, G, H, I, J, K, L, M, N, O,
+    0, 1, 0, 2, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+//  P, Q, R, S, T, U, V, W, X, Y, Z,  
+    0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//  a, b, c, d, e, f, g, h, i, j, k, l, m, n, o,
+    0, 1, 0, 2, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0,
+//  p, q, r, s, t, u, v, w, x, y, z, 
+    0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+};
+
+#define NUC_TABLE_SIZE 5
+#define PHRED_MAX 93 
+#define PHRED_LIMIT 47
+#define PHRED_TABLE_SIZE ((PHRED_LIMIT / 4) + 1)
+
+typedef uint64_t counter_t;
+
+/* Illumina reads often use a limited set of phreds rather than the full range
+   of 0-93. Putting phreds before nucleotides in the array type therefore gives
+   nice dense spots in the array where all nucleotides with the same phred sit
+   next to eachother. That leads to better cache locality. */
+typedef counter_t counttable_t[PHRED_TABLE_SIZE][NUC_TABLE_SIZE];
+
+inline uint8_t phred_to_index(uint8_t phred) {
+    if (phred > PHRED_LIMIT){
+        phred = PHRED_LIMIT;
+    }
+    return phred >> 2;
+}
+
 static struct PyModuleDef _qc_module = {
     PyModuleDef_HEAD_INIT,
     "_qc",   /* name of module */
@@ -43,6 +90,12 @@ PyInit__qc(void)
     PyTypeObject *SequenceRecord = 
         (PyTypeObject *)PyObject_GetAttrString(dnaio, "SequenceRecord");
     if (SequenceRecord == NULL) {
+        return NULL;
+    }
+    if (!PyType_CheckExact(SequenceRecord)) {
+        PyErr_Format(PyExc_RuntimeError, 
+            "SequenceRecord is not a type class but, %s", 
+            Py_TYPE(SequenceRecord)->tp_name);
         return NULL;
     }
     return m;
