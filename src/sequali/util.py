3a902d2148a71516061a9cd6ed83f83e74e7f6a2
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-07-19 07:19:46 +0200

    Add function to iterate over sequence files

--- src/sequali/util.py
+++ src/sequali/util.py
@@ -16,7 +16,7 @@
 
 import gzip
 import os
-from typing import BinaryIO, Callable, Optional
+from typing import BinaryIO, Callable, Iterator, Optional, Tuple
 
 import tqdm
 
@@ -73,3 +73,15 @@ class ProgressUpdater():
             current_position = self._get_position()
             self.tqdm.update(current_position - self.previous_file_pos)
             self.previous_file_pos = current_position
+
+
+def sequence_file_iterator(sequence_file: str) -> Iterator[Tuple[str, str]]:
+    with open(sequence_file, "rt") as seqfile:
+        for line in seqfile:
+            line = line.strip()
+            if not line:
+                continue  # ignore empty lines
+            if line.startswith("#"):
+                continue  # Use # as a comment character
+            name, sequence = line.split("\t")
+            yield name, sequence
