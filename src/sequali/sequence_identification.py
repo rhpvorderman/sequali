424c5ea4c01c43b8ea5d163e5192edcf9a89732a
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-07-19 07:15:28 +0200

    Create function to create an index

--- src/sequali/sequence_identification.py
+++ src/sequali/sequence_identification.py
@@ -14,7 +14,10 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with sequali.  If not, see <https://www.gnu.org/licenses/
 
-from typing import Optional
+import collections
+from typing import Dict, FrozenSet, Iterable, Optional, Set, Tuple
+
+DEFAULT_K = 13
 
 
 def create_upper_table():
@@ -51,6 +54,33 @@ def canonical_kmers(sequence: str, k: int):
         else:
             canonical_set.add(kmer.decode("ascii"))
     return canonical_set
+
+
+class SequenceIdentifier:
+    name: str
+    kmers: FrozenSet[str]
+    _hash: int
+    __slots__ = ["name", "kmers", "_hash"]
+
+    def __init__(self, name: str, kmers: Iterable[str]):
+        self.name = name
+        self.kmers = frozenset(kmers)
+        self._hash = hash(name) ^ hash(kmers)
+
+    def __hash__(self):
+        return self._hash
+
+
+def create_sequence_index(names_and_sequences: Iterable[Tuple[str, str]],
+                          k: int = DEFAULT_K,
+                          ) -> Dict[str, Set[SequenceIdentifier]]:
+    sequence_index = collections.defaultdict(set)
+    for name, sequence in names_and_sequences:
+        kmers = canonical_kmers(sequence, k)
+        seq_identifier = SequenceIdentifier(name, kmers)
+        for kmer in kmers:
+            sequence_index[kmer].add(seq_identifier)
+    return sequence_index
 
 
 def identify_sequence(sequence: str) -> Optional[str]:
