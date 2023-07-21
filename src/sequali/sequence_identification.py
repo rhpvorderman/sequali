da82c0878f2a29e3decff5e6cc56196b9a9131e3
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-07-21 06:30:55 +0200

    Improve build_time by creating less lists

--- src/sequali/sequence_identification.py
+++ src/sequali/sequence_identification.py
@@ -14,9 +14,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with sequali.  If not, see <https://www.gnu.org/licenses/
 
-import collections
 import sys
-from typing import Dict, FrozenSet, Iterable, List, Set, Tuple
+from typing import Dict, FrozenSet, Iterable, List, Set, Tuple, Union
 
 DEFAULT_K = 13
 
@@ -76,26 +75,42 @@ class SequenceIdentifier:
         return self._hash
 
 
-def create_sequence_index(names_and_sequences: Iterable[Tuple[str, str]],
-                          k: int = DEFAULT_K,
-                          ) -> Dict[str, List[SequenceIdentifier]]:
-    sequence_index = collections.defaultdict(list)
+def create_sequence_index(
+        names_and_sequences: Iterable[Tuple[str, str]],
+        k: int = DEFAULT_K,
+        ) -> Dict[str, Union[List[SequenceIdentifier], SequenceIdentifier]]:
+    sequence_index: Dict[str, Union[List[SequenceIdentifier], SequenceIdentifier]] = {}
     for name, sequence in names_and_sequences:
         kmers = canonical_kmers(sequence, k)
         seq_identifier = SequenceIdentifier(name, kmers)
+        # Store one sequence identifier (most common) or alternatively a list
+        # of multiple sequence identifiers. This is a bit convoluted, but it
+        # saves a lot of time as memory does not need to be allocated for a
+        # lot of one-item lists.
         for kmer in kmers:
-            sequence_index[kmer].append(seq_identifier)
-    return dict(sequence_index)
+            if kmer in sequence_index:
+                prev_entry = sequence_index[kmer]
+                if isinstance(prev_entry, list):
+                    sequence_index[kmer].append(seq_identifier)  # type: ignore
+                else:
+                    sequence_index[kmer] = [seq_identifier, prev_entry]
+            else:
+                sequence_index[kmer] = seq_identifier
+    return sequence_index
 
 
-def identify_sequence(sequence: str,
-                      sequence_index: Dict[str, List[SequenceIdentifier]],
-                      k: int = DEFAULT_K) -> Tuple[int, int, str]:
+def identify_sequence(
+        sequence: str,
+        sequence_index: Dict[str, Union[List[SequenceIdentifier], SequenceIdentifier]],
+        k: int = DEFAULT_K) -> Tuple[int, int, str]:
     kmers = canonical_kmers(sequence, k)
     candidates: Set[SequenceIdentifier] = set()
-    default_set: Set[SequenceIdentifier] = set()
     for kmer in kmers:
-        candidates.update(sequence_index.get(kmer, []))
+        matched = sequence_index.get(kmer, [])
+        if isinstance(matched, list):
+            candidates.update(matched)
+        else:
+            candidates.add(matched)
     most_matches = 0
     best_match = "No match"
     best_match_kmers = sys.maxsize
