d1d729347b7761d02953115060a007fd5cebe63c
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-08-25 13:05:03 +0200

    Add tests for header read functions

---
+++ tests/test_util.py
@@ -0,0 +1,22 @@
+import pytest
+
+from sequali.util import fastq_header_is_illumina, fastq_header_is_nanopore
+
+
+@pytest.mark.parametrize(["header", "is_illumina"], (
+    ("SIM:1:FCX:1:15:6329:1045 1:N:0:2", True),
+    ("SIM:1:FCX:1:15:6329:1045 1:Y:0:2", True),
+))
+def test_fastq_header_is_illumina(header, is_illumina):
+    assert fastq_header_is_illumina(header) == is_illumina
+
+
+@pytest.mark.parametrize(["header", "is_nanopore"], (
+    ("SIM:1:FCX:1:15:6329:1045 1:N:0:2", False),
+    ("SIM:1:FCX:1:15:6329:1045 1:Y:0:2", False),
+    ("35eb0273-89e2-4093-98ed-d81cbdafcac7 "
+     "runid=1d18d9e9682449156d70520e06571f01c4e6d2d8 sampleid=GM24185_1 "
+     "read=41492 ch=2628 start_time=2019-01-26T18:52:46Z", True)
+))
+def test_fastq_header_is_nanopore(header, is_nanopore):
+    assert fastq_header_is_nanopore(header) == is_nanopore
