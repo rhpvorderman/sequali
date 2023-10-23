147bd97ac901589410133af1ab39e4f467066d85
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-10-23 15:20:12 +0200

    Add a full metrics test

---
+++ tests/test_qc_metrics.py
@@ -0,0 +1,58 @@
+import math
+
+from sequali import A, C, G, N, T
+from sequali import FastqRecordView, QCMetrics
+from sequali import NUMBER_OF_NUCS, NUMBER_OF_PHREDS
+
+
+def view_from_sequence(sequence: str) -> FastqRecordView:
+    return FastqRecordView(
+        "name",
+        sequence,
+        "A" * len(sequence)
+    )
+
+
+def test_qc_metrics():
+    sequence = "A" * 10 + "C" * 10 + "G" * 10 + "T" * 10 + "N" * 10
+    qualities = chr(10 + 33) * 25 + chr(30 + 33) * 25
+    metrics = QCMetrics()
+    metrics.add_read(FastqRecordView("name", sequence, qualities))
+    assert metrics.max_length == len(sequence)
+    assert metrics.number_of_reads == 1
+    gc_content = metrics.gc_content()
+    assert sum(gc_content) == 1
+    assert gc_content[50] == 1
+    phred_content = metrics.phred_scores()
+    this_read_error = (10 ** -1) * 25 + (10 ** -3) * 25
+    this_read_phred = -10 * math.log10(this_read_error / len(sequence))
+    phred_index = round(this_read_phred)
+    assert phred_content[phred_index] == 1
+    assert sum(phred_content) == 1
+    phred_array = metrics.phred_count_table()
+    assert len(phred_array) == len(sequence) * NUMBER_OF_PHREDS
+    assert sum(phred_array[(10 // 4):len(phred_array):NUMBER_OF_PHREDS]) == 25
+    assert sum(phred_array[(30 // 4):len(phred_array):NUMBER_OF_PHREDS]) == 25
+    assert sum(phred_array) == len(sequence)
+    for i in range(25):
+        assert phred_array[(10 // 4) + NUMBER_OF_PHREDS * i] == 1
+    for i in range(25, 50):
+        assert phred_array[(30 // 4) + NUMBER_OF_PHREDS * i] == 1
+    base_array = metrics.base_count_table()
+    assert len(base_array) == len(sequence) * NUMBER_OF_NUCS
+    assert sum(phred_array[A: len(phred_array): NUMBER_OF_NUCS]) == 10
+    assert sum(phred_array[C: len(phred_array): NUMBER_OF_NUCS]) == 10
+    assert sum(phred_array[G: len(phred_array): NUMBER_OF_NUCS]) == 10
+    assert sum(phred_array[T: len(phred_array): NUMBER_OF_NUCS]) == 10
+    assert sum(phred_array[N: len(phred_array): NUMBER_OF_NUCS]) == 10
+    assert sum(phred_array) == len(sequence)
+    for i in range(10):
+        assert base_array[A + NUMBER_OF_NUCS * i] == 1
+    for i in range(10, 20):
+        assert base_array[C + NUMBER_OF_NUCS * i] == 1
+    for i in range(20, 30):
+        assert base_array[G + NUMBER_OF_NUCS * i] == 1
+    for i in range(30, 40):
+        assert base_array[T + NUMBER_OF_NUCS * i] == 1
+    for i in range(40, 50):
+        assert base_array[N + NUMBER_OF_NUCS * i] == 1
