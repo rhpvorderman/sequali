540b76f2532212fee0d627fa9eeeaa70f3fe39bc
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-11-07 15:56:24 +0100

    Make fingerprinter slightly faster

--- scripts/fingerprinter.py
+++ scripts/fingerprinter.py
@@ -6,8 +6,8 @@ import collections
 
 from sequali.report_modules import DuplicationCounts
 
-if __name__ == "__main__":
-    counter = collections.Counter()
+
+def fastq_file_to_hashes(fastq_file):
     with dnaio.open(sys.argv[1], open_threads=1) as reader:
         for read in reader:
             length = len(read)
@@ -15,8 +15,12 @@ if __name__ == "__main__":
                 h = hash(read.sequence)
             else:
                 h = hash(read.sequence[:16]) ^ (length >> 6) ^ hash(read.sequence[-16:])
-            counter.update((h,))
-    dupcounter = collections.Counter()
-    dupcounter.update(counter.values())
+            yield h
+
+
+if __name__ == "__main__":
+    counter = collections.Counter(fastq_file_to_hashes(sys.argv[1]))
+    dupcounter = collections.Counter(counter.values())
+    print(dict(sorted(dupcounter.items())))
     estimated_fractions = DuplicationCounts.estimated_counts_to_fractions(dupcounter.items())
     print(estimated_fractions)
