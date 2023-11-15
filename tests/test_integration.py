bc78ee7fa8cb6210b30fd758a76d326612426ddc
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-11-15 21:12:06 +0100

    Add tests for empty read

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -21,10 +21,23 @@ def test_simple_fastq(tmp_path):
 
 
 def test_empty_file(tmp_path):
-    simple_fastq = TEST_DATA / "empty.fastq"
-    sys.argv = ["", "--dir", str(tmp_path), str(simple_fastq)]
+    empty_fastq= TEST_DATA / "empty.fastq"
+    sys.argv = ["", "--dir", str(tmp_path), str(empty_fastq)]
     main()
     simple_fastq_json = tmp_path / "empty.fastq.json"
+    assert simple_fastq_json.exists()
+    result = json.loads(simple_fastq_json.read_text())
+    assert result["summary"]["maximum_length"] == 0
+    assert result["summary"]["minimum_length"] == 0
+    assert result["summary"]["total_gc_bases"] == 0
+    assert result["summary"]["total_bases"] == 0
+
+
+def test_empty_read(tmp_path):
+    empty_read_fastq = TEST_DATA / "empty_read.fastq"
+    sys.argv = ["", "--dir", str(tmp_path), str(empty_read_fastq)]
+    main()
+    simple_fastq_json = tmp_path / "empty_read.fastq.json"
     assert simple_fastq_json.exists()
     result = json.loads(simple_fastq_json.read_text())
     assert result["summary"]["maximum_length"] == 0
