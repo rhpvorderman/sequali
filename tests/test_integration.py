5861f18c339cfae0f3b86dc6d4b2eeec502131de
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-11-15 21:09:17 +0100

    Add tests and empty fastq file

---
+++ tests/test_integration.py
@@ -0,0 +1,33 @@
+import json
+import sys
+from pathlib import Path
+
+from sequali.__main__ import main
+
+TEST_DATA = Path(__file__).parent / "data"
+
+
+def test_simple_fastq(tmp_path):
+    simple_fastq = TEST_DATA / "simple.fastq"
+    sys.argv = ["", "--dir", str(tmp_path), str(simple_fastq)]
+    main()
+    simple_fastq_json = tmp_path / "simple.fastq.json"
+    assert simple_fastq_json.exists()
+    result = json.loads(simple_fastq_json.read_text())
+    assert result["summary"]["maximum_length"] == 8
+    assert result["summary"]["minimum_length"] == 7
+    assert result["summary"]["total_gc_bases"] == 4
+    assert result["summary"]["total_bases"] == 22
+
+
+def test_empty_file(tmp_path):
+    simple_fastq = TEST_DATA / "empty.fastq"
+    sys.argv = ["", "--dir", str(tmp_path), str(simple_fastq)]
+    main()
+    simple_fastq_json = tmp_path / "empty.fastq.json"
+    assert simple_fastq_json.exists()
+    result = json.loads(simple_fastq_json.read_text())
+    assert result["summary"]["maximum_length"] == 0
+    assert result["summary"]["minimum_length"] == 0
+    assert result["summary"]["total_gc_bases"] == 0
+    assert result["summary"]["total_bases"] == 0
