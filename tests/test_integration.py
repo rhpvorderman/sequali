560577b0e9c658782488d0621d74b44aae2971c9
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-11-16 15:30:55 +0100

    Make sure overrepresentation is properly tested

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -21,7 +21,7 @@ def test_simple_fastq(tmp_path):
 
 
 def test_empty_file(tmp_path):
-    empty_fastq= TEST_DATA / "empty.fastq"
+    empty_fastq = TEST_DATA / "empty.fastq"
     sys.argv = ["", "--dir", str(tmp_path), str(empty_fastq)]
     main()
     simple_fastq_json = tmp_path / "empty.fastq.json"
@@ -44,3 +44,33 @@ def test_empty_read(tmp_path):
     assert result["summary"]["minimum_length"] == 0
     assert result["summary"]["total_gc_bases"] == 0
     assert result["summary"]["total_bases"] == 0
+
+
+def test_adapters_only(tmp_path):
+    adapters_fastq = TEST_DATA / "100_illumina_adapters.fastq"
+    sys.argv = ["", "--dir", str(tmp_path),
+                "--overrepresentation-sample-every", "1",
+                str(adapters_fastq)]
+    main()
+    adapters_fastq_json = tmp_path / "100_illumina_adapters.fastq.json"
+    assert adapters_fastq_json.exists()
+    result = json.loads(adapters_fastq_json.read_text())
+    assert result["summary"]["maximum_length"] == 33
+    assert result["summary"]["minimum_length"] == 33
+    assert result["summary"]["total_gc_bases"] == 1700
+    assert result["summary"]["total_bases"] == 3300
+    adapter_content_lists = result["adapter_content"]["adapter_content"]
+    for adapter_name, quantities in adapter_content_lists:
+        if adapter_name == "Illumina Universal Adapter":
+            assert quantities == [100.0] * 33
+        assert len(quantities) == 33
+    overrepresented_sequences = result["overrepresented_sequences"]
+    assert overrepresented_sequences["total_sequences"] == 100
+    assert overrepresented_sequences["sampled_sequences"] == 100
+    assert overrepresented_sequences["total_fragments"] == 200
+    overrepr_seqs = overrepresented_sequences["overrepresented_sequences"]
+    for d in overrepr_seqs:
+        assert d["count"] == 100
+        # Illumina adapter should be in the database. All kmers should match.
+        assert d["most_matches"] == d["max_matches"]
+        assert "Illumina" in d["best_match"]
