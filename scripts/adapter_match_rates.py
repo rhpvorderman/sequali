837175eab08cc7c6fd5a230fe66daceaa2edc5f9
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-11-19 13:08:01 +0100

    Calculate accidental matches

--- scripts/adapter_match_rates.py
+++ scripts/adapter_match_rates.py
@@ -29,7 +29,7 @@ def chance_to_match(length, error_rate, allowed_errors):
     if allowed_errors < 0:
         raise ValueError(
             f"allowed_errors should be a positive number, got {allowed_errors}")
-    return sum(
+    return math.fsum(
         binominal(error_rate, length, n) for n in range(allowed_errors + 1)
     )
 
@@ -37,6 +37,18 @@ def chance_to_match(length, error_rate, allowed_errors):
 def match_probabilities(error_rates, length, allowed_errors):
     return [chance_to_match(length, error_rate, allowed_errors)
             for error_rate in error_rates]
+
+
+def chance_accidental_match(length, allowed_errors):
+    return math.fsum(
+        math.comb(length, i) / 4 ** (length - i)
+        for i in range(allowed_errors + 1)
+    )
+
+
+def false_positive_rate(probe_length, sequence_length, allowed_errors):
+    change_no_false_positive = 1 - chance_accidental_match(probe_length, allowed_errors)
+    return 1 - change_no_false_positive ** sequence_length
 
 
 def main():
@@ -49,11 +61,16 @@ def main():
     print(f"Length: {length}. Allowed errors: {allowed_errors}.")
     error_rates = (0.001, 0.01, 0.1, 0.2, 0.4, 0.5)
     print("\nChance of matching")
-    print("Error rate\t" + "\t".join(f"{e:.3f}" for e in error_rates))
-    print("Chance of match\t" + "\t".join(
-        f"{p:.3f}"
+    print("Error rate:     \t" + "\t".join(f"{e:.4f}" for e in error_rates))
+    print("Chance of match:\t" + "\t".join(
+        f"{p:.4f}"
         for p in match_probabilities(error_rates, args.length, args.allowed_errors)))
     lengths = (151, 8000, 20_000)
+    print("\nFalse positive rates")
+    print("Length:              \t" + "\t".join(str(l) for l in lengths))
+    print("False positive rate:\t" + "\t".join(
+        f"{false_positive_rate(args.length, l, args.allowed_errors):.5f}"
+        for l in lengths))
 
 
 if __name__ == "__main__":
