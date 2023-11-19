218e43ecaa2b560ea01b5dfcbe472010681a3ccf
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-11-19 10:51:56 +0100

    Add a script to determine probe lengths

---
+++ scripts/adapter_match_rates.py
@@ -0,0 +1,62 @@
+"""
+Calculate adapter match rates.
+
+For adapter probe size selection two metrics are important:
+  - The chance of a random hit on a random dna sequence of a given length.
+  - The chance the probe will be detected if it is there for a given error rate.
+
+This script calculates both allowing to make some design choices
+"""
+import argparse
+import math
+
+
+def poisson(rate, length, events):
+    """
+    Calulate the probability of a given number of events occuring given the
+    rate and an interval.
+
+    See: https://en.wikipedia.org/wiki/Poisson_distribution
+    """
+    return ((rate * length) ** events * math.exp(-rate * length)) / (
+        math.factorial(events))
+
+
+def chance_to_match(length, error_rate, allowed_errors):
+    """
+    Calculate the chance a probe of length matches a sequence with the
+    error_rate and up to allowed errors.
+    """
+    if allowed_errors < 0:
+        raise ValueError(
+            f"allowed_errors should be a positive number, got {allowed_errors}")
+    return sum(
+        poisson(error_rate, length, n) for n in range(allowed_errors + 1)
+    )
+
+
+def match_probabilities(error_rates, length, allowed_errors):
+    return [chance_to_match(length, error_rate, allowed_errors)
+            for error_rate in error_rates]
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("length", type=int)
+    parser.add_argument("allowed_errors", type=int)
+    args = parser.parse_args()
+    length = args.length
+    allowed_errors = args.allowed_errors
+    print(f"Length: {length}. Allowed errors: {allowed_errors}.")
+    error_rates = (0.001, 0.01, 0.1, 0.2, 0.4, 0.5)
+    print("\nChance of matching")
+    print("Error rate\t" + "\t".join(f"{e:.3f}" for e in error_rates))
+    print("Chance of match\t" + "\t".join(
+        f"{p:.3f}"
+        for p in match_probabilities(error_rates, args.length, args.allowed_errors)))
+    lengths = (151, 8000, 20_000)
+
+
+if __name__ == "__main__":
+    main()
+
