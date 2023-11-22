80c9962ceef32af04723969b7b5ad4d3a33a041e
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-11-22 12:52:34 +0100

    Make fingerprint length and offset settable

--- scripts/finger_print_quality.py
+++ scripts/finger_print_quality.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 
 import dnaio
@@ -11,20 +12,28 @@ def fingerprint_sequence_original(sequence: str):
     return sequence[:16] + sequence[-16:]
 
 
-def new_fingerprint(sequence: str, max_offset=32):
-    if len(sequence) < 32:
+def new_fingerprint(sequence: str, fingerprint_length=32, max_offset=32):
+    fingerprint_part_length = fingerprint_length // 2
+    if len(sequence) < fingerprint_length:
         return sequence
-    remainder = len(sequence) - 32
+    remainder = len(sequence) - fingerprint_length
     offset = max(remainder // 2, max_offset)
-    return sequence[offset: offset + 16] + sequence[-(offset + 16):-offset]
+    return sequence[offset: offset + fingerprint_part_length] + sequence[-(offset + fingerprint_part_length):-offset]
 
 
 if __name__ == "__main__":
-    expected_errors = [0 for _ in range(32 + 1)]
-    fastq = sys.argv[1]
-    with dnaio.open(fastq, mode="r", open_threads=1) as reader:
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("fastq")
+    parser.add_argument("--fingerprint-length", nargs="?", default=32, type=int)
+    parser.add_argument("--offset", nargs="?", default=32, type=int)
+    args = parser.parse_args()
+    offset = args.offset
+    fingerprint_length = args.fingerprint_length
+    expected_errors = [0 for _ in range(fingerprint_length + 1)]
+    with dnaio.open(args.fastq, mode="r", open_threads=1) as reader:
         for read in reader:  # type: dnaio.SequenceRecord
-            fingerprint_quals = new_fingerprint(read.qualities)
+            fingerprint_quals = new_fingerprint(read.qualities, fingerprint_length, offset)
             prob = 0.0
             for q in fingerprint_quals.encode("ascii"):
                 prob += QUAL_TO_PHRED[q]
