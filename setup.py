039c7a88732ad1b2c7cb17225d332955e2593181
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2024-03-15 15:35:17 +0100

    Start on using versioneer to set unique version numbers

--- setup.py
+++ setup.py
@@ -14,51 +14,16 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with sequali.  If not, see <https://www.gnu.org/licenses/
 
-from pathlib import Path
-
 from setuptools import Extension, find_packages, setup
 
 
 setup(
-    name="sequali",
-    version="0.5.0-dev",
-    description="Fast sequencing quality metrics",
-    author="Leiden University Medical Center",
-    author_email="r.h.p.vorderman@lumc.nl",
-    long_description=Path("README.rst").read_text(),
-    long_description_content_type="text/x-rst",
-    license="AGPL-3.0-or-later",
-    keywords="FASTQ sequencing quality uBAM QC nanopore illumina",
     zip_safe=False,
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    url="https://github.com/rhpvorderman/sequali",
-    classifiers=[
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: "
-        "GNU Affero General Public License v3 or later (AGPLv3+)",
-        "Topic :: Scientific/Engineering :: Bio-Informatics",
-    ],
-    python_requires=">=3.8",  # SupportsIndex requires 3.8
-    install_requires=[
-        "xopen>=1.8.0",
-        "pygal>=3.0.4",
-        "tqdm"
-    ],
     package_data={'sequali': ['*.pyi', 'py.typed',
                               'contaminants/*', 'adapters/*', 'style/*']},
     ext_modules=[
         Extension("sequali._qc", ["src/sequali/_qcmodule.c"])
     ],
-    entry_points={"console_scripts": [
-        'sequali=sequali.__main__:main',
-        'sequali-report=sequali.__main__:sequali_report'
-    ]},
 )
