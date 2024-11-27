c2eed39005ffa6ab7b601696dd20ad646ecdc893
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2024-11-27 17:09:45 +0100

    Update build machinery to support the stable ABI

--- setup.py
+++ setup.py
@@ -14,12 +14,34 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with sequali.  If not, see <https://www.gnu.org/licenses/
 
+# ABI3 building example from
+# https://github.com/joerick/python-abi3-package-sample/blob/main/setup.py
+
 from setuptools import Extension, setup
+
+from wheel.bdist_wheel import bdist_wheel
+
+
+class bdist_wheel_abi3(bdist_wheel):
+    def get_tag(self):
+        python, abi, plat = super().get_tag()
+
+        if python.startswith("cp"):
+            return "cp310", "abi3", plat
+        return python, abi, plat
 
 
 setup(
     ext_modules=[
-        Extension("sequali._qc", ["src/sequali/_qcmodule.c"]),
-        Extension("sequali._seqident", ["src/sequali/_seqidentmodule.c"])
+        Extension(
+            "sequali._qc",
+            ["src/sequali/_qcmodule.c"],
+            py_limited_api=True
+        ),
+        Extension(
+            "sequali._seqident",
+            ["src/sequali/_seqidentmodule.c"],
+            py_limited_api=True),
     ],
+    cmdclass={"bdist_wheel": bdist_wheel_abi3},
 )
